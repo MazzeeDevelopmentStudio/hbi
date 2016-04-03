e4aef08bf3bbd9a216370fac88f2065727e53113
Michael Hamson <michael@mazzee.com>
2016-04-03 22:25:56 +0200

    Removing person global config
    this is a file that should not be stored in git since its customised by the individual dev/qa staffer

--- config/global.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-DEFINE("QAHOST", "http://macdev.local:4444/wd/hub");
-DEFINE("QASERVER", "http://losethebackpain.comx");
-DEFINE("ADMINUSER", "michael@mazzee.com");
-DEFINE("ADMINPASS", "123Michael321");
-DEFINE("LOGSDIR", "/Users/Shared/qalogs");
